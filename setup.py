b90bdbe093b77a98a17d2d1f7868c7cd24a5280d
David Westerhoff <dmwesterhoff@gmail.com>
2016-08-22 13:48:05 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = ['scikit-learn']
 
 setup(
     name='lsanomaly',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/lsanomaly/lsanomaly',
     description='Least squares anomaly detection.',
     long_description=readme,
